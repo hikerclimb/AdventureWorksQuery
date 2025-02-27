50a9ab6b497142635695291564dbec41d8ffcda4
Aniket Karmarkar <karmarkaraniket@gmail.com>
2025-02-27 10:12:20 -0600

    added sql query1 and 2 from https://www.w3resource.com/sql-exercises/adventureworks/adventureworks-exercises.php

---
+++ AdventureWorksQuery1&2.sql
@@ -0,0 +1,19 @@
+SELECT TOP (1000) [BusinessEntityID]
+      ,[NationalIDNumber]
+      ,[LoginID]
+      ,[OrganizationNode]
+      ,[OrganizationLevel]
+      ,[JobTitle]
+      ,[BirthDate]
+      ,[MaritalStatus]
+      ,[Gender]
+      ,[HireDate]
+      ,[SalariedFlag]
+      ,[VacationHours]
+      ,[SickLeaveHours]
+      ,[CurrentFlag]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2022].[HumanResources].[Employee] order by JobTitle asc
+
+  select * from [AdventureWorks2022].[HumanResources].[Employee] e join [AdventureWorks2022].[Person].[Person] p on p.BusinessEntityID = e.BusinessEntityID order by LastName
