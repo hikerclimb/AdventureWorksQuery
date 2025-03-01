4a9c4d7dd9a967a7594446fd0eadbef8c6085f54
Aniket Karmarkar <karmarkaraniket@gmail.com>
2025-02-28 18:27:29 -0600

    answer key seems to be wrong

--- SQLQuery21.sql
+++ SQLQuery21.sql
@@ -1,6 +1,25 @@
-select ROW_NUMBER() OVER(ORDER BY salesytd desc, postalcode asc) as row, LastName, SUM(SalesYTD) as salesytd, PostalCode from Sales.SalesPerson sp join
+select ROW_NUMBER() OVER(PARTITION BY PostalCode ORDER BY PostalCode asc) as row, LastName, SUM(SalesYTD) as salesytd, PostalCode from Sales.SalesPerson sp join
 Person.Person p on sp.BusinessEntityID = p.BusinessEntityID
 join Person.Address a
-on a.AddressID = p.BusinessEntityID 
-group by a.AddressID, LastName, PostalCode, salesytd order by salesytd desc, PostalCode asc
+on a.AddressID = p.BusinessEntityID where SalesYTD <> 0 and TerritoryID is not NULL
+group by LastName, PostalCode, salesytd order by PostalCode asc, salesytd desc
 
+select TerritoryID from Person.Address a join Person.Person p on a.AddressID = p.BusinessEntityID 
+join Sales.SalesPerson s on s.TerritoryID = p.BusinessEntityID
+where PostalCode = '98027' and LastName = 'Jiang'
+
+SELECT 
+    ROW_NUMBER() OVER (PARTITION BY pa.PostalCode ORDER BY sp.SalesYTD DESC) AS "Row Number",
+    pp.LastName, 
+    sp.SalesYTD, 
+    pa.PostalCode
+FROM Sales.SalesPerson AS sp
+    INNER JOIN Person.Person AS pp
+        ON sp.BusinessEntityID = pp.BusinessEntityID
+    INNER JOIN Person.BusinessEntityAddress AS pba
+        ON sp.BusinessEntityID = pba.BusinessEntityID
+    INNER JOIN Person.Address AS pa
+        ON pba.AddressID = pa.AddressID
+WHERE sp.TerritoryID IS NOT NULL
+    AND sp.SalesYTD <> 0
+ORDER BY pa.PostalCode;
