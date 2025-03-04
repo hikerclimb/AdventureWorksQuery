9af98311a624a971a74846958d76afd40c00ede2
Aniket Karmarkar <karmarkaraniket@gmail.com>
2025-03-04 15:42:22 -0600

    modified query to include quartile

--- SQLQuery34.sql
+++ SQLQuery34.sql
@@ -1,6 +1,6 @@
 select FirstName, LastName, ROW_NUMBER() OVER(partition by AddressId order by postalcode) as 'Row Number', RANK() OVER(partition BY AddressId 
 order by postalcode) as Rank
-,DENSE_RANK() OVER(partition by AddressId order by postalcode desc) as 'Dense Rank', SalesYTD, PostalCode from 
+,DENSE_RANK() OVER(partition by AddressId order by postalcode desc) as 'Dense Rank', NTILE(4) OVER(order by postalcode) as Quartile, SalesYTD, PostalCode from 
 Sales.SalesPerson sp join Person.Person p on sp.BusinessEntityID = p.BusinessEntityID 
 join Person.Address a on a.AddressID = p.BusinessEntityID where TerritoryID is not null and salesytd <> 0;
 
